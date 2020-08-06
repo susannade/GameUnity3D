f1c29e554d8d812e2223edd13752d5726ae673b6
susannade <deskazuzanna@wp.pl>
2020-08-06 17:22:08 +0200

    Jumping
    added ability to jump

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Threading;
 using UnityEngine;
@@ -7,17 +8,44 @@ using UnityEngine;
 public class ThirdPersonCharacterController : MonoBehaviour
 {
     public float Speed;
+    public Rigidbody rb;
+    public bool isGrounded;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
     }
-    void PlayerMovement()
+
+    
+
+    void PlayerMovement() // odpowiada za ruch i skoki gracza
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
         Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+
     }
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.tag == "Ground") // je¿eli dodajecie coœ i chcecie zeby dalej skaka³o z tej powiechni to dodajcie tag 'Ground'
+        {
+            isGrounded = true;
+        }
+    }
+
 }

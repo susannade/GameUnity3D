b78b102686d2b615904e7228fbcc2a902a2b5f76
Mateusz Kus <43095105+dudller@users.noreply.github.com>
2020-08-08 15:02:34 +0200

    HealthBar
    first step to do health bar UI

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public gameObject player;
+    public Text score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

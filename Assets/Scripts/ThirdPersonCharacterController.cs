00841fa4c8540e7b530edf95fce937792385efb9
Mateusz Kus <tarlrasha@gmail.com>
2020-08-12 17:28:40 +0200

    Revert "Merge branch 'master' into coins-and-stars"
    This reverts commit 12be8f55718b77356f8fbc2edcd8889bdf557dee, reversing
    changes made to daf6b39667634481a0072c19f070c634f39b6e22.

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -23,29 +22,3 @@ public class ThirdPersonCharacterController : MonoBehaviour
     }
 
 }
-||||||| merged common ancestors
-=======
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Threading;
-using UnityEngine;
-
-public class ThirdPersonCharacterController : MonoBehaviour
-{
-    public float Speed;
-
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerMovement();
-    }
-    void PlayerMovement()
-    {
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
-        transform.Translate(playerMovement, Space.Self);
-    }
-}
->>>>>>> master
